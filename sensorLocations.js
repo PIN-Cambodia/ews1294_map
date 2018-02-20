dd167dc1afda3c13073bcb773ec711f41155fc4e
Blake Gardiner <33143061+blakegards@users.noreply.github.com>
2018-02-20 14:58:44 +0700

    initial draft.

---
+++ sensorLocations.js
@@ -0,0 +1,12 @@
+var sensorData = {"type":"FeatureCollection","features":[
+{"type":"Feature","id":"01","properties":{"name":"Phnom Kravanh","waterLevel":554.65, "status":"Normal"},"geometry":{"type":"Point","coordinates":[103.755745,12.354825]}},
+{"type":"Feature","id":"02","properties":{"name":"Kampot","waterLevel":234.43, "status":"Warning"},"geometry":{"type":"Point","coordinates":[104.163102,10.677277]}},
+{"type":"Feature","id":"03","properties":{"name":"Stueng Saen","waterLevel":100.2, "status":"Warning"},"geometry":{"type":"Point","coordinates":[104.888036,12.714603]}},
+{"type":"Feature","id":"04","properties":{"name":"Siem Reap","waterLevel":123.55, "status":"Normal"},"geometry":{"type":"Point","coordinates":[103.850553, 13.316804]}},
+{"type":"Feature","id":"04","properties":{"name":"Kampong Chhnang","waterLevel":345.67, "status":"Alert"},"geometry":{"type":"Point","coordinates":[104.680781,12.266333]}},
+{"type":"Feature","id":"04","properties":{"name":"Sangkat Preah Ponlea","waterLevel":748.34, "status":"Normal"},"geometry":{"type":"Point","coordinates":[102.981859,13.574203]}},
+{"type":"Feature","id":"04","properties":{"name":"Khum Mean Chey","waterLevel":230.8, "status":"Normal"},"geometry":{"type":"Point","coordinates":[102.74672,12.70083]}},
+{"type":"Feature","id":"04","properties":{"name":"Battambang","waterLevel":404.5, "status":"Normal"},"geometry":{"type":"Point","coordinates":[102.58517,13.020919]}},
+{"type":"Feature","id":"04","properties":{"name":"New 1","waterLevel":-1, "status":"Inactive"},"geometry":{"type":"Point","coordinates":[104.911386,11.533671]}},
+{"type":"Feature","id":"04","properties":{"name":"New 2","waterLevel":-1, "status":"Inactive"},"geometry":{"type":"Point","coordinates":[104.928231,11.557204]}}
+]};
