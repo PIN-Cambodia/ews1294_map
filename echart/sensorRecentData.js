a9bbacaee35b3b03c5442946c7640073e3566dc0
Blake Gardiner <33143061+blakegards@users.noreply.github.com>
2018-02-23 17:28:12 +0700

    updates

---
+++ echart/sensorRecentData.js
@@ -0,0 +1,10 @@
+var sensorRecentData = {"type":"from_my_database","sensorID":"101065684","sensorName":"Siem Reap","warningThreshold":"330","alertThreshold":"350","record":[
+{"type":"record","id":"01","timestamp":"01/02/2018 00:00:00","height":"302","voltage":"5000"},
+{"type":"record","id":"02","timestamp":"02/02/2018 00:00:00","height":"305","voltage":"5000"},
+{"type":"record","id":"03","timestamp":"03/02/2018 00:00:00","height":"307","voltage":"5000"},
+{"type":"record","id":"04","timestamp":"04/02/2018 00:00:00","height":"309","voltage":"5000"},
+{"type":"record","id":"05","timestamp":"05/02/2018 00:00:00","height":"315","voltage":"5000"},
+{"type":"record","id":"06","timestamp":"06/02/2018 00:00:00","height":"317","voltage":"5000"},
+{"type":"record","id":"07","timestamp":"07/02/2018 00:00:00","height":"320","voltage":"5000"},
+{"type":"record","id":"08","timestamp":"08/02/2018 00:00:00","height":"315","voltage":"5000"},
+]};
