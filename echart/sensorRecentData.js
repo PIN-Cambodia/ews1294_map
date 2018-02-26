c32a6ebc2655848c4e866c4aba40991edb9e8201
Blake Gardiner <33143061+blakegards@users.noreply.github.com>
2018-02-26 14:59:09 +0700

    bug fixes for icon locations and popups

--- echart/sensorRecentData.js
+++ echart/sensorRecentData.js
@@ -7,4 +7,10 @@ var sensorRecentData = {"type":"from_my_database","sensorID":"101065684","sensor
 {"type":"record","id":"06","timestamp":"06/02/2018 00:00:00","height":"317","voltage":"5000"},
 {"type":"record","id":"07","timestamp":"07/02/2018 00:00:00","height":"320","voltage":"5000"},
 {"type":"record","id":"08","timestamp":"08/02/2018 00:00:00","height":"315","voltage":"5000"},
+{"type":"record","id":"09","timestamp":"09/02/2018 00:00:00","height":"319","voltage":"5000"},
+{"type":"record","id":"10","timestamp":"10/02/2018 00:00:00","height":"320","voltage":"5000"},
+{"type":"record","id":"11","timestamp":"11/02/2018 00:00:00","height":"327","voltage":"5000"},
+{"type":"record","id":"12","timestamp":"12/02/2018 00:00:00","height":"335","voltage":"5000"},
+{"type":"record","id":"13","timestamp":"13/02/2018 00:00:00","height":"335","voltage":"5000"},
+{"type":"record","id":"14","timestamp":"14/02/2018 00:00:00","height":"340","voltage":"5000"}
 ]};
